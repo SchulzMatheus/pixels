9a3272782379c43095aa5ca8631a1fd139c7cb6a
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-06 19:38:42 -0300

    criando função para gerar uma cor aleatoria em hexadeximal e testando-a

---
+++ script.js
@@ -0,0 +1,9 @@
+function randomizeColor(){
+    const hexadeximal = '0123456789ABCDEF'
+    let randomColor = '#'
+    for(let i = 0; i < 6; i++) {
+        randomColor += hexadeximal[Math.floor(Math.random() * 16)]
+    }
+    return randomColor;
+}
+console.log(randomizeColor())
